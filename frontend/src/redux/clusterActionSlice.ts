61c867d88f604770b6126763b4fe9f830c2b7509
Evangelos Skopelitis <eskopelitis@microsoft.com>
2024-07-17 19:10:32 -0400

    frontend: Prevent success pop-up on invalid operations
    This change ensures that invalid resource operations (e.g. changing a
    resource kind) do not trigger a success notification. Previously, the
    app would indicate a successful attempt to apply changes even when it is
    unsuccessful. Now when trying to perform an invalid apply/edit
    operation, instead the editor will reappear with the error message and a
    failure notification.
    
    Fixes: #2123
    
    Signed-off-by: Evangelos Skopelitis <eskopelitis@microsoft.com>

--- frontend/src/redux/clusterActionSlice.ts
+++ frontend/src/redux/clusterActionSlice.ts
@@ -242,7 +242,7 @@ export const executeClusterAction = createAsyncThunk(
         if (controller.signal.aborted) {
           return rejectWithValue('Action cancelled');
         }
-        callback();
+        await callback();
         dispatchSuccess();
       } catch (err) {
         if ((err as Error).message === 'Action cancelled' || controller.signal.aborted) {
